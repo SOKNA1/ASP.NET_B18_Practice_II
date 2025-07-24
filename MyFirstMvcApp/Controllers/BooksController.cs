ea66a916cee54039d447a38cfdf907fda54359dd
MSI <soknanorn1@gmail.com>
2025-07-24 19:13:14 +0700

    Homework2

---
+++ MyFirstMvcApp/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return Content("This is the book index page.");
+        }
+
+      
+        public IActionResult All()
+        {
+            return Content("<h1>Here is a list of ALL books!</h1>", "text/html");
+            
+        }
+
+      
+        public IActionResult Details(int id)
+        {
+            
+            return Content($"Showing details for book with ID: {id}");
+        }
+
+        
+        public IActionResult OldCatalog()
+        {
+            
+            return RedirectToAction("All", "Books");
+        }
+    }
+}
